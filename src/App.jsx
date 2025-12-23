933f7161b08835cd1a2215cfcb5ee224c2cbc862
Nie <22130317@st.hcmuaf.edu.vn>
2025-12-23 22:46:40 +0700

    add CartPage

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./styles/main.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Home from "./components/Home";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from "./components/CartPage";
+
+function App() {
+  return (
+    // <>
+    //   <Home />
+    // </>
+
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<CartPage />} />
+          </Routes>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
