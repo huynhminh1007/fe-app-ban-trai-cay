85284a82421babe5404e89a98b5f7a7bfede37ff
Nie <165691316+NieNieNe@users.noreply.github.com>
2026-01-05 10:30:33 +0700

    create checkoutPage (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Home from "./components/Home";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CartPage from "./components/CartPage";
+<<<<<<< HEAD
 import ProductDetailPage from "./Pages/ProductDetailPage";
+=======
+import CheckoutPage from "./components/CheckoutPage";
+>>>>>>> 26fe22c (create checkoutPage)
 
 function App() {
   return (
@@ -18,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/checkout" element={<CheckoutPage />}/>
       </Routes>
     </BrowserRouter>
   );
