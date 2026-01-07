38b0cdc1dcc65e536dda6707663c891fed25073d
LeQuangHuy-22130100 <22130100@st.hcmuaf.edu.vn>
2026-01-07 11:02:17 +0700

    Separate the components into individual blocks

--- src/Pages/ProductDetailPage.jsx
+++ src/Pages/ProductDetailPage.jsx
@@ -24,34 +24,51 @@ function ProductDetailPage() {
       {/* header */}
       {/* <Header /> */}
 
-      {/* Main Produtc */}
-      <main style={{ backgroundColor: '#fff', paddingBottom: '30px' }}>
-        <div className="container"
-          style={{ maxWidth: '1200px', margin: '20px auto', padding: '0 15px' }}
-        >
-          <div className="product-detail-layout"
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: '40px',
-              alignItems: 'flex-start'
-            }}>
-
-            <div style={{ flex: '1 1 400px', maxWidth: '500px' }}>
-              <ProductGallery />
-            </div>
-
-            <div style={{ flex: '1 1 400px' }}>
-              <ProductInfor />
-            </div>
-
-          </div>
-
-          <ProductDescription data={currentProduct} />
-
-          <ProductSimilar />
-        </div>
-      </main>
+      {/* Main Product */}
+       <main style={{ backgroundColor: '#f4f4f4', paddingBottom: '30px' }}>
+         
+         <div className="container"
+           style={{ 
+             maxWidth: '1200px', 
+             margin: '20px auto', 
+             padding: '18px 15px',
+             display: 'flex',      
+             flexDirection: 'column', 
+             gap: '30px'            // khoảng gap giữa các components
+           }}
+         >
+           
+           <div className="product-detail-layout"
+             style={{
+               display: 'flex',
+               flexWrap: 'wrap',
+               gap: '40px',
+               alignItems: 'flex-start',
+               backgroundColor: '#fff', 
+               padding: '20px',
+               borderRadius: '8px'
+             }}>
+ 
+             <div style={{ flex: '1 1 400px', maxWidth: '500px' }}>
+               <ProductGallery />
+             </div>
+ 
+             <div style={{ flex: '1 1 400px' }}>
+               <ProductInfor />
+             </div>
+ 
+           </div>
+ 
+           <div style={{ backgroundColor: '#fff', padding: '20px', borderRadius: '8px' }}>
+              <ProductDescription data={currentProduct} />
+           </div>
+ 
+           <div style={{ backgroundColor: '#fff', padding: '20px', borderRadius: '8px' }}>
+              <ProductSimilar />
+           </div>
+           
+         </div>
+       </main>
 
       {/* footer */}
       {/* <Footer /> */}
