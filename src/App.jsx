e5c73c07a4679d22bcc3405bff122a0842fd8d74
Nie <22130317@st.hcmuaf.edu.vn>
2026-01-18 00:46:37 +0700

    add postDetail

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CartPage from "./components/CartPage";
 import ProductDetailPage from "./Pages/ProductDetailPage";
 import CheckoutPage from "./components/CheckoutPage";
 import ProductListPage from "./components/ProductListPage";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/products" element={<ProductListPage />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
       </Routes>
     </BrowserRouter>
   );
